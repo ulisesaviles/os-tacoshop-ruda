0e1c9a732dc694145f44b370159a93b3f1681d10
Rene <rene.nunez@cetys.edu.mx>
2021-12-01 19:55:37 -0800

    Added logs, cleaned the code

--- app/src/objects/taquero.js
+++ app/src/objects/taquero.js
@@ -1,23 +1,27 @@
-import Handler from "../config/ordersStateHandler";
+import OrdersHandler from "../config/ordersStateHandler";
 
-const Taquero = (type, ordersSetter) => {
+const Taquero = (name, canWorkOn, ordersSetter, logsHandler) => {
   // Handlers and helpers
   const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-  const handler = Handler(ordersSetter, type);
+  const ordersHandler = OrdersHandler(ordersSetter, name);
 
   // Attributes
   let queue = [];
 
   // Functions
   const getOrders = () => {
-    return handler.getAllOrders()[type];
+    return ordersHandler.getAllOrders()[name];
   };
 
   const insertToQueue = (newElements) => {
     queue = [...getOrders(), ...newElements];
-    handler.setOrders(queue);
-    // console.log(`Successfully setted orders to ${JSON.stringify(getOrders())}`);
+    ordersHandler.setOrders(queue);
+    log(`Inserted ${newElements.length} element(s) succesfully`);
     return queue;
+  };
+
+  const log = (message) => {
+    logsHandler.log(`Taquero de ${name}:`, message);
   };
 
   const startWorking = async () => {
@@ -31,16 +35,15 @@ const Taquero = (type, ordersSetter) => {
   const workOnNextOrder = async () => {
     await timeout(1000);
     queue = getOrders();
-    // console.log(queue);
-    // console.log(`Taquero de ${type} finalizó una orden`);
     queue.shift();
-    handler.setOrders(queue);
+    ordersHandler.setOrders(queue);
+    log(`Finished an order, ${queue.length} left`);
     return queue.length;
   };
 
   // Actual object
   return {
-    type,
+    type: name,
     insertToQueue,
     getQueueSize: () => getOrders().length,
     workOnNextOrder,
