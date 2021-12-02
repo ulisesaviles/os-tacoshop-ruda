af535f758768f0535e50306dfe7549bcaab097a2
Rene <rene.nunez@cetys.edu.mx>
2021-12-01 19:54:30 -0800

    Created allocation and balance obj

---
+++ app/src/config/allocationAndBalancing.js
@@ -0,0 +1,24 @@
+const Handler = (taqueros, quesadilleros, taqueroTypes) => {
+  const getOrders = (cuantity) => {
+    let res = [];
+    for (let i = 0; i < cuantity; i++) {
+      res.push(`Order ${i}`);
+    }
+    return res;
+  };
+
+  const insertTaquero = (taqueroType, ordersCuantity) => {
+    taqueros[taqueroTypes.indexOf(taqueroType)].insertToQueue(
+      getOrders(ordersCuantity)
+    );
+  };
+
+  const start = () => {
+    // Insert 10 orders and start working
+    for (let i = 0; i < taqueros.length; i++) {
+      taqueros[i].insertToQueue(getOrders(10));
+    }
+  };
+  return { start, insertTaquero };
+};
+export default Handler;
