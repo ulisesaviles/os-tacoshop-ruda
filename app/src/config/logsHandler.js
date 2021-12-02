4fa2ee1ace9e1ae3b04439cb634cc258dc09b2b9
Rene <rene.nunez@cetys.edu.mx>
2021-12-01 21:53:28 -0800

    Cleaned the code

--- app/src/config/logsHandler.js
+++ app/src/config/logsHandler.js
@@ -3,7 +3,6 @@ const Handler = (setLogsFunction) => {
     let logs = JSON.parse(localStorage.getItem("logs"));
     logs.unshift({ title, message, time: Date.now() });
     setLogsFunction(logs);
-    console.log(logs);
     localStorage.setItem("logs", JSON.stringify(logs));
   };
 
