64758ebfd77487b6da0081fb664e1187b037628e
David <david.dennis@cetys.edu.mx>
2021-12-03 15:04:02 -0800

    Added table

---
+++ app/src/components/table.js
@@ -0,0 +1,33 @@
+// Styles
+import "../App.css";
+
+const Table = (props) => {
+  const { data } = props;
+  const headers =
+    data != null && data.length >= 1 ? Object.keys(data[0]) : null;
+
+  return headers ? (
+    <div className="table">
+      <div className="tableRow">
+        {headers.map((header) => (
+          <div className="cell cellHeader">
+            <p className="cellText cellTextHeader">{header}</p>
+          </div>
+        ))}
+      </div>
+      {data.map((row) => (
+        <div className="tableRow">
+          {headers.map((header) => (
+            <div className={`cell cell${data.indexOf(row) % 2}`}>
+              <p className="cellText">{row[header]}</p>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  ) : (
+    <p className="noOrders">(No orders)</p>
+  );
+};
+
+export default Table;
