db3e0de03e2bdd509ad75dc52e7e7422b1bd1159
Abner <abner20015@hotmail.com>
2021-12-03 02:01:13 -0800

    Added quesadilleros and real orders

--- app/src/objects/taquero.js
+++ app/src/objects/taquero.js
@@ -23,6 +23,29 @@ const Taquero = (
     return ordersHandler.getAllOrders()[name];
   };
 
+  const getQuesadillasInStock = () => {
+    return metadataHandler.getMetadata()[name].quesadillasInStock;
+  };
+
+  const getPartToParticipate = (parts) => {
+    for (let i = 0; i < parts.length; i++) {
+      const part = parts[i];
+      if (
+        canWorkOn.includes(part.meat) &&
+        part.status !== "done" &&
+        (part.type !== "quesadilla" || getQuesadillasInStock() >= 1)
+      ) {
+        return [part, i];
+      }
+    }
+    return [null, null];
+  };
+
+  const giveQuesadilla = () => {
+    const newStock = metadataHandler.getMetadata()[name].quesadillasInStock + 1;
+    metadataHandler.setMetadata("quesadillasInStock", newStock);
+  };
+
   const insertToQueue = (newElements) => {
     queue = [...getOrders(), ...newElements];
     ordersHandler.setOrders(queue);
@@ -35,36 +58,88 @@ const Taquero = (
     logsHandler.log(`Taquero de ${name}:`, message);
   };
 
-  const putFillings = async (order) => {
-    // MAKE IT WORKKK
-    const fillings = ["salsa", "guacamole", "cilantro", "cebolla"];
-    await metadataHandler.consumeFillings(fillings);
+  const putFillings = async (part) => {
+    await metadataHandler.consumeFillings(part.ingredients);
+  };
+
+  const reAllocateOrder = async (order) => {
+    const orders = JSON.parse(localStorage.getItem("ordersToReAllocate"));
+    localStorage.setItem(
+      "ordersToReAllocate",
+      JSON.stringify([...orders, order])
+    );
+  };
+
+  const rest1Second = async () => {
+    await timeout(1000);
+    let meta = metadataHandler.getMetadata()[name];
+    meta.rest.timeRested++;
+    metadataHandler.setMetadata("rest", meta.rest);
   };
 
   const startWorking = async () => {
-    if (getOrders().length === 0) return;
     while (true) {
-      const itemsLeft = await workOnNextOrder();
-      if (itemsLeft === 0) break;
+      if (getOrders().length === 0) await rest1Second();
+      else await workOnNextOrder();
+      if (!JSON.parse(localStorage.getItem("RUDAIsWorking"))) break;
     }
   };
 
   const workOnNextOrder = async () => {
     // Get orders
     queue = getOrders();
-    // MAke the first one
-    const order = queue.shift();
-    metadataHandler.setMetadata("workingOn", order);
-    // Taco time
-    metadataHandler.useTortilla();
-    await timeout(1000);
-    // Put fllings
-    await putFillings(order);
+    // Make the first one's corresponding parts
+    const order = queue[0];
+    // Get a part to participate in
+    let [part, partIndex] = getPartToParticipate(order.orden);
+    if (
+      part === null ||
+      (part.type === "quesadilla" && getQuesadillasInStock() === 0)
+    ) {
+      reAllocateOrder(queue.shift());
+      await rest1Second();
+      return;
+    }
+    const start = Date.now();
+    // Start working on it
+    part.status = "working";
+    metadataHandler.setMetadata("workingOn", part.part_id);
+    // Make the tacos
+    for (let i = 0; part.quantity > part.finished_products; i++) {
+      // If quesdilla, use 1
+      if (part.type === "quesadilla") {
+        if (getQuesadillasInStock() > 0) {
+          const newStock =
+            metadataHandler.getMetadata()[name].quesadillasInStock - 1;
+          metadataHandler.setMetadata("quesadillasInStock", newStock);
+        } else {
+          part.status = "open";
+          break;
+        }
+      }
+      // Taco time
+      metadataHandler.useTortilla();
+      await timeout(1000);
+      // Put fllings
+      await putFillings(part);
+      // Sum
+      part.finished_products += 1;
+      if (part.quantity === part.finished_products) part.status = "done";
+    }
+    part.status = part.status === "done" ? part.status : "open";
     // Set it
+    queue[0].orden[partIndex] = part;
+    queue[0].response.push({
+      who: `Taquero de ${name}`,
+      when: new Date().toISOString(),
+      what: `Made ${part.finished_products} ${part.meat} ${part.type} (part ${part.part_id})`,
+      time: Date.now() - start,
+    });
+    reAllocateOrder(queue.shift());
     ordersHandler.setOrders(queue);
-    metadataHandler.madeTaco();
+    metadataHandler.madeTacos(part.quantity);
     // Log it
-    log(`Finished an order, ${queue.length} left`);
+    log(`Finished part "${part.part_id}" ${queue.length} left`);
     // Return it
     return queue.length;
   };
@@ -77,6 +152,9 @@ const Taquero = (
     workOnNextOrder,
     getOrders,
     startWorking,
+    canWorkOn,
+    getQuesadillasInStock,
+    giveQuesadilla,
   };
 };
 
