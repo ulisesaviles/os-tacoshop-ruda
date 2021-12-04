e55fcb86b58280eaca1009da5ea197a8b5982925
David <david.dennis@cetys.edu.mx>
2021-12-03 19:03:52 -0800

    Now it uses ready quesadillas

--- app/src/objects/quesadillero.js
+++ app/src/objects/quesadillero.js
@@ -5,15 +5,18 @@ const Quesadillero = (setMetadataFunction, taqueros) => {
   // Handlers and helpers inicialization
   const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   const metadataHandler = MetadataHandler(setMetadataFunction, "quesadillero");
+  const taquerosQuesadillasSpace = 5;
+
+  const getReadyQuesadillas = () => {
+    return metadataHandler.getMetadata()["quesadillero"].quesadillasReady;
+  };
 
   const saveQuesadilla = () => {
-    metadataHandler.setMetadata(
-      "quesadillasReady",
-      metadataHandler.getMetadata()["quesadillero"].quesadillasReady + 1
-    );
+    metadataHandler.setMetadata("quesadillasReady", getReadyQuesadillas() + 1);
   };
 
   const start = async () => {
+    watchForSpaceToPutQuesadillas();
     while (true) {
       if (!JSON.parse(localStorage.getItem("RUDAIsWorking"))) break;
       // Use Tortillas
@@ -21,6 +24,10 @@ const Quesadillero = (setMetadataFunction, taqueros) => {
         "tortillas",
         metadataHandler.getMetadata()["quesadillero"].tortillas - 1
       );
+      // Rest
+      let rest = metadataHandler.getMetadata()["quesadillero"].rest;
+      rest.untilNeeded -= 1;
+      metadataHandler.setMetadata("rest", rest);
       await timeout(5000); // Fucking long time to make a tortilla with cheese
       let min = {
         index: null,
@@ -30,13 +37,41 @@ const Quesadillero = (setMetadataFunction, taqueros) => {
         const taquero = taqueros[i];
         if (
           taquero.getQuesadillasInStock() < min.quantity &&
-          taquero.getQuesadillasInStock() < 5
+          taquero.getQuesadillasInStock() < taquerosQuesadillasSpace
         )
           min = { index: i, quantity: taquero.getQuesadillasInStock() };
       }
       if (min.index !== null) {
         taqueros[min.index].giveQuesadilla();
       } else saveQuesadilla();
+    }
+  };
+
+  const watchForSpaceToPutQuesadillas = async () => {
+    while (true) {
+      if (!JSON.parse(localStorage.getItem("RUDAIsWorking"))) break;
+      let quesadillasReady = getReadyQuesadillas();
+      if (quesadillasReady > 0) {
+        // Iterate every taquero
+        for (let i = 0; i < taqueros.length; i++) {
+          const taquero = taqueros[i];
+          while (
+            taquero.getQuesadillasInStock() < taquerosQuesadillasSpace &&
+            quesadillasReady > 0
+          ) {
+            // Use ready quesadilla
+            metadataHandler.setMetadata(
+              "quesadillasReady",
+              getReadyQuesadillas() - 1
+            );
+            // Give it away
+            taquero.giveQuesadilla();
+            // rewrite quesadillas ready
+            quesadillasReady = getReadyQuesadillas();
+          }
+        }
+      }
+      await timeout(100);
     }
   };
 
