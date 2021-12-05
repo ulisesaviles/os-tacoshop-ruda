a98627a4728627027693c99e6a07fc2d6d81f59f
Ulises <ulises.aviles@cetys.edu.mx>
2021-12-04 17:17:18 -0800

    100 top logs

--- app/src/config/logsHandler.js
+++ app/src/config/logsHandler.js
@@ -2,6 +2,8 @@ const Handler = (setLogsFunction) => {
   const log = (title, message) => {
     let logs = JSON.parse(localStorage.getItem("logs"));
     logs.unshift({ title, message, time: Date.now() });
+
+    if (logs.length > 100) logs.pop();
     setLogsFunction(logs);
     localStorage.setItem("logs", JSON.stringify(logs));
   };
